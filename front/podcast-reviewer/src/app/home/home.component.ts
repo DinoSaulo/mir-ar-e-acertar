5e3350d8c304e7dbcb5bdeaf8fa04da2756903ad
JecaTatu <mayhhara@hotmail.com.br>
2021-04-24 14:21:29 -0300

    home is ready to integrate

--- front/podcast-reviewer/src/app/home/home.component.ts
+++ front/podcast-reviewer/src/app/home/home.component.ts
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  public message: string;
+  public withMusic: boolean;
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  typeValidator(files) {
+    this.message = '';
+    if (files.length === 0) {
+      return;
+    }
+    let mimeType = files[0].type;
+
+    if (mimeType.match(/audio\/*/) == null) {
+      this.withMusic = false;
+      this.message = "Só suportamos audio";
+      return;
+    }
+    this.withMusic = true;
+  }
+
+  uploadPod(){}
 
 }
